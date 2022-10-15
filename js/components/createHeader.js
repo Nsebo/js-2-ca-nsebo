3c56d34f50dcb04311c686a232edd3117cf51b0e
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-10-15 13:17:05 +0200

    created single page and made few corrected to the header

--- js/components/createHeader.js
+++ js/components/createHeader.js
@@ -14,19 +14,21 @@ function createHeader() {
                 <a href="/index.html" class="${
                   pathname === '/index.html'
                     ? 'hover:underline text-blue-600 '
-                    : ''}">Home</a>
-            </li>
-            <li class="p-8"> <span>Hello ${name}</span> </li>
+                    : ''}">Home</a></li>
             <li class="p-8"><a href="/post-page.html" class="${
-              pathname === '/post-page.html' ? 'hover:underline' : ''
+              pathname === '/post-page.html' ? 'hover:underline text-blue-600' : ''
             }">Create post</a></li>
+            <li class="p-8"><a href="/single-post.html" class="${
+        pathname === '/single-post.html' ? 'hover:underline text-blue-600' : ''
+    }">My Posts</a></li> 
+       <li class="p-8"> <span>Hello ${name}</span> </li>
 `;
   } else {
     authLink = `  <li class="p-8"><a href="/signup.html" class="${
-      pathname === '/signup.html' ? 'hover:underline ' : ''
+      pathname === '/signup.html' ? 'hover:underline text-blue-600 ' : ''
     }">SignUp</a></li>
             <li class="p-8"><a href="/login.html" class="${
-              pathname === '/login.html' ? 'hover:underline' : ''
+              pathname === '/login.html' ? 'hover:underline text-blue-600' : ''
             }">Log In</a></li>
             `;
   }
