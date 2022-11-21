edd64a5f8c6dd98d1978c16ce073f40b86ebf9d4
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2022-11-21 21:09:33 +0100

    Update signUp.js

--- js/signUp.js
+++ js/signUp.js
@@ -1,6 +1,6 @@
-import { API_BASE_URL, CREATE_USER_URL, LOGIN_USER_URL } from "./settings/api";
-import { validateEmail, validatePassword } from "./utils/validation";
-import { saveToken } from "./utils/storage";
+import {API_BASE_URL, CREATE_USER_URL, LOGIN_USER_URL} from "./settings/api";
+import {validateEmail, validatePassword} from "./utils/validation";
+import {saveToken} from "./utils/storage";
 
 const form = document.querySelector("#signup");
 
@@ -19,92 +19,92 @@ console.log(confirmPassword);
 const confirmPasswordError = document.querySelector("#confirmPasswordError");
 console.log(confirmPasswordError);
 const confirmPasswordErrorNotMatching = document.querySelector(
-  "#confirmPasswordErrorNotMatching"
+    "#confirmPasswordErrorNotMatching"
 );
 console.log(confirmPasswordErrorNotMatching);
 
 const formErrorMessage = document.querySelector("#form-error-message");
 
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let isUserName = false;
-  if (userName.value.trim().length > 0) {
-    userNameError.classList.add("hidden");
-    isUserName = true;
-  } else {
-    userNameError.classList.remove("hidden");
-  }
+    event.preventDefault();
+    let isUserName = false;
+    if (userName.value.trim().length > 0) {
+        userNameError.classList.add("hidden");
+        isUserName = true;
+    } else {
+        userNameError.classList.remove("hidden");
+    }
 
-  let isEmail = false;
-  if (email.value.trim().length > 0) {
-    emailErrorMessage.classList.add("hidden");
-    isEmail = true;
-  } else {
-    emailErrorMessage.classList.remove("hidden");
-  }
+    let isEmail = false;
+    if (email.value.trim().length > 0) {
+        emailErrorMessage.classList.add("hidden");
+        isEmail = true;
+    } else {
+        emailErrorMessage.classList.remove("hidden");
+    }
 
-  let isValidEmail = false;
-  if (email.value.trim().length && validateEmail(email.value) === true) {
-    emailErrorNotValid.classList.add("hidden");
-    isValidEmail = true;
-  } else if (email.value.trim().length && validateEmail(email.value) !== true) {
-    emailErrorNotValid.classList.remove("hidden");
-  }
+    let isValidEmail = false;
+    if (email.value.trim().length && validateEmail(email.value) === true) {
+        emailErrorNotValid.classList.add("hidden");
+        isValidEmail = true;
+    } else if (email.value.trim().length && validateEmail(email.value) !== true) {
+        emailErrorNotValid.classList.remove("hidden");
+    }
 
-  let isPassword = false;
-  if (password.value.trim().length >= 8) {
-    passwordErrorMessage.classList.add("hidden");
-    isPassword = true;
-  } else {
-    passwordErrorMessage.classList.remove("hidden");
-  }
+    let isPassword = false;
+    if (password.value.trim().length >= 8) {
+        passwordErrorMessage.classList.add("hidden");
+        isPassword = true;
+    } else {
+        passwordErrorMessage.classList.remove("hidden");
+    }
 
-  let isConfirmPassword = false;
-  if (confirmPassword.value.trim().length >= 8) {
-    confirmPasswordError.classList.add("hidden");
-    isConfirmPassword = true;
-  } else {
-    confirmPasswordError.classList.remove("hidden");
-  }
+    let isConfirmPassword = false;
+    if (confirmPassword.value.trim().length >= 8) {
+        confirmPasswordError.classList.add("hidden");
+        isConfirmPassword = true;
+    } else {
+        confirmPasswordError.classList.remove("hidden");
+    }
 
-  let isValidPasswordMatch = false;
-  isValidPasswordMatch = validatePassword();
+    let isValidPasswordMatch = false;
+    isValidPasswordMatch = validatePassword();
 
-  let isFormValid =
-    isUserName && isEmail && isValidEmail && isPassword && isConfirmPassword;
+    let isFormValid =
+        isUserName && isEmail && isValidEmail && isPassword && isConfirmPassword;
 
-  if (isFormValid) {
-    console.log("validate SUCCEED ");
+    if (isFormValid) {
+        console.log("validate SUCCEED ");
 
-    const userData = {
-      name: userName.value,
-      email: email.value,
-      password: password.value,
-    };
+        const userData = {
+            name: userName.value,
+            email: email.value,
+            password: password.value,
+        };
 
-    // API CALL
-    console.log(LOGIN_USER_URL);
+        // API CALL
+        console.log(LOGIN_USER_URL);
 
-    (async function createUser() {
-      const response = await fetch(`${CREATE_USER_URL}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
-      if (response.ok) {
-        const data = await response.json();
-        location.href = "../login.html";
-      } else {
-        const err = await response.json();
-        const message = `Error: ${err.message}`;
-        throw new Error(message);
-      }
-    })().catch((err) => {
-      formErrorMessage.innerHTML = `${err.message}`;
-    });
-  } else {
-    console.log("validation failed");
-  }
+        (async function createUser() {
+            const response = await fetch(`${CREATE_USER_URL}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                // location.href = "../login.html";
+            } else {
+                const err = await response.json();
+                const message = `Error: ${err.message}`;
+                throw new Error(message);
+            }
+        })().catch((err) => {
+            formErrorMessage.innerHTML = `${err.message}`;
+        });
+    } else {
+        console.log("validation failed");
+    }
 });
